3d0c160cade90f13f077cd45d4f85ee42bc62ff2
liangsky <640634387@qq.com>
2023-09-24 21:02:54 +0800

    test: fix demo preload file path

--- examples/demo/src/main/main-window.ts
+++ examples/demo/src/main/main-window.ts
@@ -31,7 +31,7 @@ function createMainWindow() {
     height: 600,
     webPreferences: {
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload-main.js'),
     },
   });
   context.mainWindow = mainWindow;
